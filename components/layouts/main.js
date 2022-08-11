404a756208a74e28e82538a75e31da23120c4d98
cophi <pzarindast@gmail.com>
2022-08-11 13:29:21 +0200

    fixed favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,11 +15,11 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Cophi's homepage" />
+        <meta name="description" content="cophi - homepage" />
         <meta name="author" content="Cophi" />
         <meta name="author" content="cophi" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         <meta name="twitter:title" content="Cophi" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@_cophi_" />
@@ -29,7 +29,7 @@ const Main = ({ children, router }) => {
         <meta name="og:title" content="Cophi" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
-        <title>Cophi's website</title>
+        <title>cophi - homepage</title>
       </Head>
 
       <NavBar path={router.asPath} />
