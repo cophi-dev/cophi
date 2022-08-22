c68cea459169b0fc307c9d25ecfb009c36a64224
cophi <pzarindast@gmail.com>
2022-08-22 12:48:03 +0200

    added projects; fixed github link

--- pages/works.js
+++ pages/works.js
@@ -3,8 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
+import thumbFrontendPortfolio from '../public/images/works/frontend-portfolio_eyecatcher.png'
+import thumbColors from '../public/images/works/colors_eyecatch.png'
 import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
 import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
 import thumbMargelo from '../public/images/works/margelo_eyecatch.png'
@@ -15,7 +15,7 @@ import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
 import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
 
 const Works = () => (
-  <Layout title="Works">
+  <Layout title="Projects">
     <Container>
       <Heading as="h3" fontSize={20} mb={4}>
         web3 projects
@@ -23,18 +23,13 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
-          </WorkGridItem>
-        </Section>
-        <Section>
           <WorkGridItem
-            id="walknote"
-            title="walknote"
-            thumbnail={thumbWalknote}
+            id="colors"
+            title="colors. nft mint page"
+            thumbnail={thumbColors}
           >
-            Music recommendation app for iOS
+            Mint page to claim randomly generated and 100% onchain NFTs of
+            colors. Ruuning on Rinkeby testnet.
           </WorkGridItem>
         </Section>
 
@@ -65,10 +60,13 @@ const Works = () => (
       </Section>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem id="margelo" thumbnail={thumbMargelo} title="Margelo">
-            A website of the elite app development and contracting agency based
-            in Austria
+        <Section>
+          <WorkGridItem
+            id="portfolio"
+            title="portfolio 1.0"
+            thumbnail={thumbFrontendPortfolio}
+          >
+            A website to showcase some of my web2.0 experiences.
           </WorkGridItem>
         </Section>
         <Section delay={0.3}>
